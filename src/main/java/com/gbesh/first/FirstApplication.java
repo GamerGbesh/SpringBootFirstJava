2935cc8433426ff79c3da67779a93a0d75464eba
Gbesh <gbesh.12@gmail.com>
2025-07-28 19:36:53 +0000

    Updated environment variables

--- src/main/java/com/gbesh/first/FirstApplication.java
+++ src/main/java/com/gbesh/first/FirstApplication.java
@@ -12,6 +12,7 @@ public class FirstApplication {
 		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
 		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("SPRING_JPA_HIBERNATE", dotenv.get("SPRING_JPA_HIBERNATE"));
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
