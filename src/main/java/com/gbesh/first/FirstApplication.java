a9dda352c50f06c14e684be5340e9d14a681db49
Gbesh <gbesh.12@gmail.com>
2025-07-28 19:33:38 +0000

    Changed to using environment variables for the important connections

--- src/main/java/com/gbesh/first/FirstApplication.java
+++ src/main/java/com/gbesh/first/FirstApplication.java
@@ -2,11 +2,16 @@ package com.gbesh.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class FirstApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
